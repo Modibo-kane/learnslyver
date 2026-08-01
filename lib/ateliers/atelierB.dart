87909b695adbfffcf9d177798ef961654418b2d2
Modibo Kane <kaneferme1@gmail.com>
2026-08-01 14:33:55 +0000

    AtelierB pratique

--- lib/ateliers/atelierB.dart
+++ lib/ateliers/atelierB.dart
@@ -10,20 +10,19 @@ class SpotifyPage extends StatelessWidget {
       body: CustomScrollView(
         physics: BouncingScrollPhysics(),
         slivers: [
-          // 1. SliverAppBar
+          // 1. SliverApp
           SliverAppBar(
             expandedHeight: 300,
             pinned: true,
             stretch: true,
-            backgroundColor: Color(0xff202020),
-            flexibleSpace: FlexibleSpaceBar(
+            backgroundColor: Color(0xFF202020),
+            flexibleSpace:  FlexibleSpaceBar(
               centerTitle: true,
-              title: Text("Techno Bunker",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                  ),
+              title: Text(
+                "Tecno Bunker",
+                style: TextStyle(
+                    color: Colors.white
+                ),
               ),
               stretchModes: [
                 StretchMode.zoomBackground,
@@ -33,31 +32,37 @@ class SpotifyPage extends StatelessWidget {
                 fit: StackFit.expand,
                 children: [
                   Image.asset("assets/album.jpeg", fit: BoxFit.cover,),
-                  DecoratedBox(
-                    decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Color(0xFF121212)
-                    ])
+                  DecoratedBox(decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.transparent,
+                            Color(0xFF121212)
+                          ]
+                      )
                   ),
                   )
                 ],
               ),
             ),
+
           ),
+
 
           // 2. SliverList
           SliverList(
               delegate: SliverChildBuilderDelegate(
-                  (context, index){
+                      (context, index){
                     return ListTile(
-                      leading: Text("${index + 1}",style: TextStyle(color: Colors.grey)),
-                      title: Text("Track Title $index", style: TextStyle(color: Colors.white),),
-                      subtitle: Text("Various Artistes,", style: TextStyle(color: Colors.grey)),
-                      trailing: Icon(Icons.more_vert, color: Colors.grey),
+                      // premier element de nos liste
+                      leading: Text("${index+1}", style: TextStyle(color: Colors.grey)),
+                      // title
+                      title: Text("Soound $index", style: TextStyle(color: Colors.white),),
+                      // le soustitre
+                      subtitle: Text("Various Artistes", style: TextStyle(color: Colors.grey)),
+                      // la fin de la liste (l'icon des trois points)
+                      trailing: Icon(Icons.more_vert, color: Colors.grey,),
                     );
                   }, childCount: 25
               )
